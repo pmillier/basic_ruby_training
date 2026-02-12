61f6d56796bd47674b7dad047bd530b92921eaa5
Pierre Millier <pmillier@Pierres-MacBook-Air.local>
2026-02-12 11:39:03 +0100

    basic array manipulation

---
+++ fundamentals/array.rb
@@ -0,0 +1,29 @@
+
+
+
+arr = Array['Apple', 'Orange', 'Banana']
+
+puts arr
+
+
+puts "Please add a fruit"
+
+newFruit = gets.chomp
+
+arr << newFruit
+
+
+puts arr.sort
+
+
+puts "Type a fruit name to delete it"
+
+rmFruit = gets.chomp
+
+
+if (arr.include? rmFruit)
+	arr.delete(rmFruit)
+	puts arr
+else
+	puts "Wrong pick, 404 fruit not found"
+end
